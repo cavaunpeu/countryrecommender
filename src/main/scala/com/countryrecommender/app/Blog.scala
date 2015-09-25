ff181cece66416c1af4f2f0325a01f359c945ffb
William Wolf <wwolf@shopkeep.com>
2015-09-25 17:32:49 -0400

    add message explaining that weve provided similar countries; add bounce to graph generation

--- src/main/scala/com/countryrecommender/app/Blog.scala
+++ src/main/scala/com/countryrecommender/app/Blog.scala
@@ -11,9 +11,10 @@ class Blog extends CountryrecommenderStack {
         contentType="text/html"
 
         if (params.contains("country")) {
+            /* log country to database! */
             val country = params("country").split(" ").map(_.capitalize).mkString(" ")
             val country_sims = new RetrieveCountrySimilarities(country).run()
-            ssp("result", "country" -> params.get("country"), "country_sims" -> country_sims)
+            ssp("result", "country" -> country, "country_sims" -> country_sims)
         } else {
             ssp("query")
         }
